9f4e7d891d9402093071dddedf7f20ddf6074b1a
rexsteroxy <rexsteroxy22@gmail.com>
2020-03-16 19:17:13 -0400

    implemented the main game functionalities

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Quizzler());
+void main() => runApp(BooleanGame());
 
-class Quizzler extends StatelessWidget {
+class BooleanGame extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.grey.shade900,
+        backgroundColor: Colors.blueAccent,
         body: SafeArea(
           child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10.0),
-            child: QuizPage(),
+            padding: EdgeInsets.symmetric(horizontal: 15.0),
+            child: GamePage(),
           ),
         ),
       ),
@@ -19,12 +19,70 @@ class Quizzler extends StatelessWidget {
   }
 }
 
-class QuizPage extends StatefulWidget {
+class GamePage extends StatefulWidget {
   @override
-  _QuizPageState createState() => _QuizPageState();
+  _GamePageState createState() => _GamePageState();
 }
 
-class _QuizPageState extends State<QuizPage> {
+class _GamePageState extends State<GamePage> {
+  List<Widget> listIcon = [];
+  List<String> questions = [
+    'The Developer of this Boolean Game is Amadi Austin Chukwuemeka',
+    'Is he a Software Engineer',
+    'He is Not a funny man',
+    'He loves Dogs',
+    'He loves Food'
+  ];
+  List<bool> answers = [true, true, false, false, true];
+
+  int questionNumber = 0;
+
+  void checkAnswerTrue(){
+    if (questionNumber < questions.length) {
+      if (answers[questionNumber] == true) {
+        listIcon.add(
+          Icon(
+            Icons.check,
+            color: Colors.white,
+          ),
+        );
+      } else {
+        listIcon.add(
+          Icon(
+            Icons.close,
+            color: Colors.black,
+          ),
+        );
+      }
+    }
+
+    if (questionNumber < questions.length - 1) {
+      questionNumber++;
+    }
+  }
+  void checkAnswerFalse(){
+    if (questionNumber < questions.length) {
+      if (answers[questionNumber] == false) {
+        listIcon.add(
+          Icon(
+            Icons.check,
+            color: Colors.white,
+          ),
+        );
+      } else {
+        listIcon.add(
+          Icon(
+            Icons.close,
+            color: Colors.black,
+          ),
+        );
+      }
+    }
+    if (questionNumber < questions.length - 1) {
+      questionNumber++;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,61 +95,93 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.black,
+              color: Colors.white,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 25.0,
+                ),
+              ),
+              onPressed: () {
+                if(listIcon.length == questions.length){
+                  setState(() {
+                    listIcon.clear();
+                    listIcon.add(
+                      Text(
+                        'Game Finished!',
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 25.0,
+                        ),
+                      ),
+                    );
+                  });
+
+                }
+                  setState (() {
+                    checkAnswerTrue ();
+                  });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.black,
+              child: Text(
+                'False',
                 style: TextStyle(
                   fontSize: 25.0,
                   color: Colors.white,
                 ),
               ),
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
-              child: Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                ),
-              ),
               onPressed: () {
-                //The user picked true.
+                if(listIcon.length == questions.length){
+                  setState(() {
+                    listIcon.clear();
+                    listIcon.add(
+                      Text(
+                        'Game Finished!',
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 25.0,
+                        ),
+                      ),
+                    );
+                  });
+                }
+                if(listIcon.length <= questions.length) {
+                  setState (() {
+                    checkAnswerFalse();
+                  });
+                }
               },
             ),
           ),
         ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
-              child: Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                ),
-              ),
-              onPressed: () {
-                //The user picked false.
-              },
-            ),
-          ),
+        Row(
+          children: listIcon,
         ),
-        //TODO: Add a Row here as your score keeper
+        //Add a Row here as your score keeper
       ],
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
